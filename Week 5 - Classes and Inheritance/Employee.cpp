c87272fe571a0e3d8afaeac464505a786ac391f8
Lee Cottrell <44240965+leecottrellPTC@users.noreply.github.com>
2023-05-16 09:39:04 -0400

    week 5 day 2
    inheritance, arrays of class, vectors IN class

---
+++ Week 5 - Classes and Inheritance/Employee.cpp
@@ -0,0 +1,52 @@
+#include "Employee.h"
+
+Employee::Employee():Person()
+{
+
+}
+
+Employee::~Employee()
+{
+
+}
+Employee::Employee(string fname, string lname, 
+        string title, double sal, 
+        vector<string> list):Person(fname, lname)
+        {
+            this->setJobTitle(title);
+            salary = sal;
+            skills = list;
+        }
+
+Employee::Employee(string fname, 
+    string lname, string title, double sal):Person(fname, lname)
+    {
+        //jobTitle = title;
+        this->setJobTitle(title);
+        salary = sal;
+    }
+
+   //either way
+    Employee::Employee(Person peep, 
+        string title, double sal):Person(peep.getFname(), peep.getLname())
+    {
+        //Person::fname = peep.getFname();
+    }
+
+  /*  Employee::Employee(Person peep, 
+        string title, double sal)//:Person(peep.getFname(), peep.getLname())
+    {
+        Person::fname = peep.getFname();
+    }
+*/
+
+string Employee::to_string(){
+    char * salOutput;
+    string output = Person::to_string();    //use parent to_string
+    output += "Job title " + jobTitle;
+    std::sprintf(salOutput, "$.2f", salary);//two decimal places
+    output += salOutput;
+    //to_string(salary)
+
+    return output;
+}
